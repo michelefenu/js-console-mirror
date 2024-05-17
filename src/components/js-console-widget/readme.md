85d54ac1afc9985ccaf65b0c419db38ceeb95b45
michelefenu <michelefenu@gmail.com>
2024-05-17 23:31:47 +0200

    Add targets for Angular, React and Vue

---
+++ src/components/js-console-widget/readme.md
@@ -0,0 +1,10 @@
+# js-console-widget
+
+
+
+<!-- Auto Generated Below -->
+
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
--- stencil.config.ts
+++ stencil.config.ts
@@ -1,4 +1,9 @@
 import { Config } from '@stencil/core';
+import { reactOutputTarget } from '@stencil/react-output-target';
+import { angularOutputTarget, ValueAccessorConfig } from '@stencil/angular-output-target';
+import { vueOutputTarget } from '@stencil/vue-output-target';
+
+const angularValueAccessorBindings: ValueAccessorConfig[] = [];
 
 export const config: Config = {
   namespace: 'js-console-widget',
@@ -19,6 +24,20 @@ export const config: Config = {
       type: 'www',
       serviceWorker: null, // disable service workers
     },
+    reactOutputTarget({
+      componentCorePackage: 'js-console-widget',
+      proxiesFile: '../react-component-library/src/components.ts',
+    }),
+    angularOutputTarget({
+      componentCorePackage: 'js-console-widget',
+      directivesProxyFile: '../angular-component-library/src/directives/proxies.ts',
+      directivesArrayFile: '../angular-component-library/src/directives/index.ts',
+      valueAccessorConfigs: angularValueAccessorBindings,
+    }),
+    vueOutputTarget({
+      componentCorePackage: 'js-console-widget',
+      proxiesFile: '../vue-component-library/src/proxies.ts',
+    }),
   ],
   testing: {
     browserHeadless: "new",

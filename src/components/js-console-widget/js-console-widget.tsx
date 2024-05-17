951ed9f83cbb7444c6bd2529d257612733745039
michelefenu <michelefenu@gmail.com>
2024-05-17 22:46:54 +0200

    Add error types

--- src/components/js-console-widget/js-console-widget.tsx
+++ src/components/js-console-widget/js-console-widget.tsx
@@ -1,4 +1,4 @@
-import { Component, State, h } from '@stencil/core';
+import { Component, State, h, Element } from '@stencil/core';
 
 enum ErrorLevel {
   info = 'info',
@@ -21,6 +21,9 @@ interface Log {
 export class JsConsoleWidget {
   @State() logs: Log[] = [];
 
+  @Element() el: HTMLElement;
+  private logContainer: HTMLElement;
+
   private originalLog = console.log;
   private originalError = console.error;
   private originalWarn = console.warn;
@@ -29,8 +32,28 @@ export class JsConsoleWidget {
 
   componentWillLoad() {
     console.log = (...args) => {
-      this.logs = [...this.logs, ...args.map(arg => ({ level: ErrorLevel.log, message: String(arg)}))];
+      this.logs = [...this.logs, ...args.map(arg => ({ level: ErrorLevel.log, message: String(arg) }))];
       this.originalLog.apply(console, args);
+    };
+
+    console.error = (...args) => {
+      this.logs = [...this.logs, ...args.map(arg => ({ level: ErrorLevel.error, message: String(arg) }))];
+      this.originalError.apply(console, args);
+    };
+
+    console.warn = (...args) => {
+      this.logs = [...this.logs, ...args.map(arg => ({ level: ErrorLevel.warn, message: String(arg) }))];
+      this.originalWarn.apply(console, args);
+    };
+
+    console.info = (...args) => {
+      this.logs = [...this.logs, ...args.map(arg => ({ level: ErrorLevel.info, message: String(arg) }))];
+      this.originalInfo.apply(console, args);
+    };
+
+    console.debug = (...args) => {
+      this.logs = [...this.logs, ...args.map(arg => ({ level: ErrorLevel.debug, message: String(arg) }))];
+      this.originalDebug.apply(console, args);
     };
   }
 
@@ -42,13 +65,22 @@ export class JsConsoleWidget {
     console.debug = this.originalDebug;
   }
 
+  componentDidUpdate() {
+    // Scroll to the bottom of the log container
+    if (this.logContainer) {
+      this.logContainer.scrollTop = this.logContainer.scrollHeight;
+    }
+  }
+
   render() {
     return (
-      <div class="console">
+      <div>
         <div class="title">CONSOLE OUTPUT</div>
-        <div class="log-section">
+        <div class="log-section" ref={el => this.logContainer = el}>
           {this.logs.map(log => (
-            <div class="log"><span class={log.level}>{`[${log.level.toLocaleUpperCase()}] ${log.message}`}</span></div>
+            <div class="log-line">
+              <span class={`log-level ${log.level}`}>{`[${log.level.toUpperCase()}]`}</span> {`${log.message}`}
+            </div>
           ))}
         </div>
       </div>

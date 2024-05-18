b544a1cae7ca7cb7fc69f054059df841313d10e0
michelefenu <michelefenu@gmail.com>
2024-05-18 16:03:44 +0200

    Fix stencil config

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,9 +1,4 @@
 import { Config } from '@stencil/core';
-import { reactOutputTarget } from '@stencil/react-output-target';
-import { angularOutputTarget, ValueAccessorConfig } from '@stencil/angular-output-target';
-import { vueOutputTarget } from '@stencil/vue-output-target';
-
-const angularValueAccessorBindings: ValueAccessorConfig[] = [];
 
 export const config: Config = {
   namespace: 'js-console-mirror',
@@ -23,22 +18,7 @@ export const config: Config = {
     {
       type: 'www',
       serviceWorker: null, // disable service workers
-    },
-    reactOutputTarget({
-      componentCorePackage: 'js-console-mirror',
-      proxiesFile: '../react-component-library/src/components.ts',
-    }),
-    angularOutputTarget({
-      componentCorePackage: 'js-console-mirror',
-      outputType: "standalone",
-      directivesProxyFile: '../angular-component-library/src/directives/proxies.ts',
-      directivesArrayFile: '../angular-component-library/src/directives/index.ts',
-      valueAccessorConfigs: angularValueAccessorBindings,
-    }),
-    vueOutputTarget({
-      componentCorePackage: 'js-console-mirror',
-      proxiesFile: '../vue-component-library/src/proxies.ts',
-    }),
+    }
   ],
   testing: {
     browserHeadless: "new",
